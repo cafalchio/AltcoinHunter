c566327fd1b268194e7c4ff7e2f33271541b36c6
cafalchio <mcafalchio@gmail.com>
2024-11-08 22:51:38 +0000

    changed project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cryptofinder",
+    name="AltcoinHunter",
     version="0.1",
     packages=find_packages(),
     description="A tool to find new potential cryptos",
